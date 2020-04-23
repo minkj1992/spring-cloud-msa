d23e5183fe0ecaed7a44af1ab568b785a9217e4a
minkj1992 <minkj1992@gmail.com>
2020-04-23 14:33:07 +0900

    add Lombok and Connect sub-projects by rest

--- display/src/main/java/com/elevenst/api/DisplayController.java
+++ display/src/main/java/com/elevenst/api/DisplayController.java
@@ -1,17 +1,22 @@
 package com.elevenst.api;
 
+import com.elevenst.api.service.ProductRemoteService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/displays")
 public class DisplayController {
 
+    private final ProductRemoteService productRemoteService;
+
     @GetMapping(path = "/{displayId}")
     public String getDisplayDetail(@PathVariable String displayId) {
-        String productInfo = "[unknown]";
+        String productInfo = productRemoteService.getProductInfo("1111");
         return String.format("[display id = %s at %s %s ]", displayId, System.currentTimeMillis(), productInfo);
     }
 }
